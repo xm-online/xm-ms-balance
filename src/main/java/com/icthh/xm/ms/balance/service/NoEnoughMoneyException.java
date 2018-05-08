741e158270f9e0aae2c484f78dffafb771173d09
ssenko <sergeysenja1992@gmail.com>
2018-05-08 12:08:17 +0300

    fix tests

--- src/main/java/com/icthh/xm/ms/balance/service/NoEnoughMoneyException.java
+++ src/main/java/com/icthh/xm/ms/balance/service/NoEnoughMoneyException.java
@@ -1,6 +1,7 @@
 package com.icthh.xm.ms.balance.service;
 
 import static com.google.common.collect.ImmutableMap.of;
+import static java.lang.String.valueOf;
 
 import com.icthh.xm.commons.exceptions.BusinessException;
 import lombok.Getter;
@@ -13,7 +14,7 @@ public class NoEnoughMoneyException extends BusinessException {
     private final BigDecimal currentAmount;
 
     public NoEnoughMoneyException(Long balanceId, BigDecimal currentAmount) {
-        super("error.no.enough.many", "No enough many", of("balanceId", balanceId.toString(), "currentAmount", currentAmount.toString()));
+        super("error.no.enough.many", "No enough many", of("balanceId", valueOf(balanceId), "currentAmount", valueOf(currentAmount)));
         this.balanceId = balanceId;
         this.currentAmount = currentAmount;
     }
