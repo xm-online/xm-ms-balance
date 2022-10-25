2e10ce28260409c2c9c0e32152ec86aa4cf34240
emurenko <evgeniy.murenko@jevera.software>
2022-10-25 15:16:02 +0300

    added LepServiceFactory lepServices  in LepContext

--- src/main/java/com/icthh/xm/ms/balance/config/LepContext.java
+++ src/main/java/com/icthh/xm/ms/balance/config/LepContext.java
@@ -1,10 +1,8 @@
 package com.icthh.xm.ms.balance.config;
 
-
-import com.icthh.xm.commons.config.client.service.TenantConfigService;
 import com.icthh.xm.commons.lep.BaseProceedingLep;
 import com.icthh.xm.commons.lep.spring.LepThreadHelper;
-import com.icthh.xm.commons.permission.service.PermissionCheckService;
+import com.icthh.xm.commons.lep.spring.lepservice.LepServiceFactory;
 import com.icthh.xm.commons.security.XmAuthenticationContext;
 import com.icthh.xm.commons.tenant.TenantContext;
 import com.icthh.xm.ms.balance.service.BalanceHistoryService;
@@ -22,6 +20,7 @@ public class LepContext {
     public TenantContext tenantContext;
     public Object methodResult;
 
+    public LepServiceFactory lepServices;
     public LepServices services;
 
     public static class LepServices {
