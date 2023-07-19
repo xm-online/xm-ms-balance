b67960eab8a923e071032f2096da390522482490
izhyvchyn <izhyvchyn@itsmartflex.com>
2023-07-19 10:57:18 +0300

    return list of imports

--- src/main/java/com/icthh/xm/ms/balance/domain/BalanceChangeEvent.java
+++ src/main/java/com/icthh/xm/ms/balance/domain/BalanceChangeEvent.java
@@ -1,14 +1,29 @@
 package com.icthh.xm.ms.balance.domain;
 
 import com.icthh.xm.ms.balance.service.OperationType;
-import lombok.*;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import static javax.persistence.CascadeType.ALL;
 import static javax.persistence.EnumType.STRING;
