41f3fec996520dd915c2a1438698b39f1bc430ee
viktorpavlenko <vitek.pavfurious@gmail.com>
2026-07-29 16:45:14 +0300

    Migrate lep key resolver

--- src/main/java/com/icthh/xm/ms/balance/service/BalanceFieldKeyResolver.java
+++ src/main/java/com/icthh/xm/ms/balance/service/BalanceFieldKeyResolver.java
@@ -1,5 +1,6 @@
 package com.icthh.xm.ms.balance.service;
 
+import com.icthh.xm.lep.api.LepKeyResolver;
 import com.icthh.xm.lep.api.LepMethod;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
@@ -7,17 +8,18 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
-public class BalanceFieldKeyResolver extends AppendLepKeyResolver {
+public class BalanceFieldKeyResolver implements LepKeyResolver {
+
     @Override
-    protected List<String> getAppendSegments(LepMethod method) {
+    public List<String> segments(LepMethod method) {
         String field = method.getParameter("field", String.class);
         return List.of(toDashSeparatedString(field));
     }
 
     private static String toDashSeparatedString(String value) {
         return StringUtils.join(
-            StringUtils.splitByCharacterTypeCamelCase(value), '-'
-            )
-            .toUpperCase();
+                        StringUtils.splitByCharacterTypeCamelCase(value), '-'
+                )
+                .toUpperCase();
     }
 }
